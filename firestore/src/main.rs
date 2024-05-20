3b64e3562974f724acff51a5d5a043d923231dbf
monda00 <monda0524@gmail.com>
2024-05-20 18:05:33 +0900

    Update firestore

--- firestore/src/main.rs
+++ firestore/src/main.rs
@@ -13,11 +13,25 @@ struct MyTestStructure {
     some_num: i64
 }
 
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct MyParentStructure {
+    some_id: String,
+    some_string: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize)]
+struct MyChildStructure {
+    some_id: String,
+    another_string: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let db = FirestoreDb::new( & config_env_var("PROJECT_ID") ? ).await?;
 
     const TEST_COLLECTION_NAME: &'static str = "test";
+    const TEST_PARENT_COLLECTION_NAME: &'static str = "nested-test";
+    const TEST_CHILD_COLLECTION_NAME: &'static str = "test-childs";
 
     let my_struct = MyTestStructure {
         some_id: "test-1".to_string(),
@@ -71,7 +85,38 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     .await?;
     */
 
+    let parent_struct = MyParentStructure {
+        some_id: "test-parent".to_string(),
+        some_string: "Test".to_string(),
+    };
 
+    // Creating a parent doc
+    db.fluent()
+        .insert()
+        .into(TEST_PARENT_COLLECTION_NAME)
+        .document_id(&parent_struct.some_id)
+        .object(&parent_struct)
+        .execute()
+        .await?;
+
+    // Creating a child doc
+    let child_struct = MyChildStructure {
+        some_id: "test-child".to_string(),
+        another_string: "TestChild".to_string(),
+    };
+
+    // The doc path where we store our childs
+    let parent_path = db.parent_path(TEST_PARENT_COLLECTION_NAME, parent_struct.some_id)?;
+
+    // Create a child doc
+    db.fluent()
+        .insert()
+        .into(TEST_CHILD_COLLECTION_NAME)
+        .document_id(&child_struct.some_id)
+        .parent(&parent_path)
+        .object(&child_struct)
+        .execute()
+        .await?;
 
     Ok(())
 }
