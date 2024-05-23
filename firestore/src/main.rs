7b65f2d51ccec3bee8850fa7d6988858b558fca8
monda00 <monda0524@gmail.com>
2024-05-23 20:32:25 +0900

    Update firestore

--- firestore/src/main.rs
+++ firestore/src/main.rs
@@ -42,37 +42,37 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Create
     let object_returned: MyTestStructure = db.fluent()
-    .insert()
-    .into(TEST_COLLECTION_NAME)
-    .document_id( & my_struct.some_id)
-    .object( & my_struct)
-    .execute()
-    .await?;
+        .insert()
+        .into(TEST_COLLECTION_NAME)
+        .document_id( & my_struct.some_id)
+        .object( & my_struct)
+        .execute()
+        .await?;
     println!("Returned object: {:?}", object_returned);
 
     // Update or Create
     // (Firestore supports creating documents with update if you provide the document ID).
     let object_updated: MyTestStructure = db.fluent()
     .update()
-    .fields(paths!(MyTestStructure::{some_num, one_more_string}))
-    .in_col(TEST_COLLECTION_NAME)
-    .document_id( & my_struct.some_id)
-    .object( & MyTestStructure {
-        some_num: my_struct.some_num + 1,
-        one_more_string: "updated-value".to_string(),
-            ..my_struct.clone()
-    })
-    .execute()
-    .await?;
+        .fields(paths!(MyTestStructure::{some_num, one_more_string}))
+        .in_col(TEST_COLLECTION_NAME)
+        .document_id( & my_struct.some_id)
+        .object( & MyTestStructure {
+            some_num: my_struct.some_num + 1,
+            one_more_string: "updated-value".to_string(),
+                ..my_struct.clone()
+        })
+        .execute()
+        .await?;
     println!("Updated object: {:?}", object_updated);
 
     // Get object by id
     let find_it_again: Option<MyTestStructure> = db.fluent()
-    .select()
-    .by_id_in(TEST_COLLECTION_NAME)
-    .obj()
-    .one( & my_struct.some_id)
-    .await?;
+        .select()
+        .by_id_in(TEST_COLLECTION_NAME)
+        .obj()
+        .one( & my_struct.some_id)
+        .await?;
     println!("Found object: {:?}", find_it_again);
 
     // Delete data
